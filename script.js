651de920d14f750c0a3d5dcd082c407ae12621f5
Glitch (elfin-radical-actor) <none>
2022-03-13 05:15:55 +0000

    🔘🦘 Updated with Glitch

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 // global constants
-const clueHoldTime = 1000; //how long to hold each clue's light/sound
+var clueHoldTime = 1000; //how long to hold each clue's light/sound
 const cluePauseTime = 333; //how long to pause in between clues
 const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
+const clueDecreasedTime = 2;
+const patternElements = 5;
 //Global Variables
 var pattern = [6, 6, 5, 5, 1, 1, 5, 4, 4, 7, 7 , 2, 2, 3];
+var patternType = 0;
 var progress = 0; 
 var gamePlaying = false;
 var tonePlaying = false;
 var volume = 0.5;  //must be between 0.0 and 1.0
 var guessCounter = 0;
 var gameStrikes = 0;
+
+
 
 function startGame(){
     //initialize game variables
@@ -20,7 +25,23 @@ function startGame(){
   // swap the Start and Stop buttons
   document.getElementById("startBtn").classList.add("hidden");
   document.getElementById("stopBtn").classList.remove("hidden");
-  playClueSequence()
+  patternType = getRandomIntForPattern(2);
+  console.log("patternType is: " + patternType);
+  if (patternType == 1){
+    randomPattern();
+    playClueSequence();
+  }else
+    playClueSequence();
+}
+  
+function randomPattern(){
+  pattern = []
+  var j = 0;
+  while (j < patternElements){
+    pattern[j] = getRandomIntSoundPattern(1, 7)
+    j++;
+  }
+  console.log(pattern);
 }
 
 function stopGame(){
@@ -31,7 +52,6 @@ function stopGame(){
   document.getElementById("stopBtn").classList.add("hidden");
   document.getElementById("startBtn").classList.remove("hidden");
 }
-
 
 // Sound Synthesis Functions
 const freqMap = {
@@ -44,6 +64,17 @@ const freqMap = {
   7: 330,
 
 }
+  
+function getRandomIntForPattern(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getRandomIntSoundPattern(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
 function playTone(btn,len){ 
   o.frequency.value = freqMap[btn]
   g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
@@ -60,11 +91,14 @@ function startTone(btn){
     g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
     context.resume()
     tonePlaying = true
+    if (btn == pattern[guessCounter])
+      document.getElementById("star"+btn).classList.remove("hidden");
   }
 }
-function stopTone(){
+function stopTone(btn){
   g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
   tonePlaying = false
+  document.getElementById("star"+btn).classList.add("hidden");
 }
 
 // Page Initialization
@@ -93,16 +127,22 @@ function playSingleClue(btn){
   }
 }
 function playClueSequence(){
+  console.log("pattern is: " + pattern);
   guessCounter = 0;
   context.resume()
   let delay = nextClueWaitTime; //set delay to initial wait time
   for(let i=0;i<=progress;i++){ // for each clue that is revealed so far
+    console.log("pattern at: " + pattern[i]);
     console.log("play single clue: " + pattern[i] + " in " + delay + "ms")
     setTimeout(playSingleClue,delay,pattern[i]) // set a timeout to play that clue
-    delay += clueHoldTime;
+    console.log("cludeHoldTime is at: " + clueHoldTime)
+    clueHoldTime -= clueDecreasedTime;
+    delay += clueHoldTime 
     delay += cluePauseTime;
   }
 }
+  
+
 function loseGame(){
   stopGame();
   alert("Game Over. You lost.");
@@ -110,7 +150,7 @@ function loseGame(){
 
 function winGame(){
   stopGame();
-  alert("Game Over. Congrats, you won!");
+  alert("Game over. Congrats, you won!");
 }
 
 function guess(btn){
@@ -143,7 +183,7 @@ function guess(btn){
   else{
     if (gameStrikes < 2){
       gameStrikes++;
-      alert("Strike "+gameStrikes+"!")
+      alert("Strike "+gameStrikes+"! Let's play the pattern again.")
       playClueSequence();
     }
     else
